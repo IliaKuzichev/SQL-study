406a4ed5f4a591c6ec6fff63fd8d075e58821d52
IliaKuzichev <72195701+IliaKuzichev@users.noreply.github.com>
2022-11-30 15:20:17 +0700

    Update SQL left join.sql

--- SQL left join.sql
+++ SQL left join.sql
@@ -1,3 +1,4 @@
+--Задача №1. Выведите список городок клиенты из которых не сделали ни одного заказа.
 select t1.City
 from Customers as t1 left join Orders as t2 on t1.CustomerID = t2.CustomerID
 where t2.OrderID is null
